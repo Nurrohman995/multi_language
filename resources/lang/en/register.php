eac49faf4c52885acc4ebaf4f2337f93477e0d1c
nur rohman <nurrohmannur110@gmail.com>
2019-03-01 08:43:34 +0700

    multi language laravel 5,*

---
+++ resources/lang/en/register.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'Register'         => 'Register',
+    'Name'             => 'Name',
+    'Email'            => 'Email',
+    'Password'         => 'Password',
+    'Confirm Password' => 'Confirm Password',
+];
---
+++ resources/lang/id/register.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'Register'         => 'Registrasi',
+    'Name'             => 'Nama',
+    'Email'            => 'Email',
+    'Password'         => 'Password',
+    'Confirm Password' => 'Konfirmasi password',
+];
